ffc06ccb37877a65d8b4f19c1550b605fedc6814
Happa1 <2025.manaha.ueda@uwcisak.jp>
2024-02-19 19:37:11 +0900

    move directory

--- quizes/quiz_050.py
+++ quizes/quiz_050.py
@@ -1,2 +1,9 @@
 class Fligt:
     def __init__(self, flight_numbers, origin, destination, demonstrate_time):
+        self.flight_numbers = flight_numbers
+        self.origin = origin
+        self.destination = destination
+        self.demonstrate_time = demonstrate_time
+
+
+    def get_duration(self, duration):
